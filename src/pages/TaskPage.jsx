48a137c2353d09d1be609c4a6da3e727f52a0929
welkerfernandes <welkerfc@icloud.com>
2024-12-09 17:17:55 -0300

    correção da tela de tarefa

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -8,7 +8,7 @@ function TaskPage() {
   const navigate = useNavigate();
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px] space-y-4">
+      <div className="w-[500px] mx-auto space-y-4">
         <div className="flex justify-center relative">
           <button
             onClick={() => navigate(-1)}
